02ee10f881f31d833500bde5caa98b53fa358e3d
Frederik Haagensen <frehaa@hotmail.dk>
2018-02-11 23:20:52 +0100

    Add work for own F# RayTracer

--- F#/main.fsx
+++ F#/main.fsx
@@ -1,40 +1,31 @@
 #r @"D:\Workspace\RayTracer\F#\vector.dll"
+#r @"D:\Workspace\RayTracer\F#\color.dll"
 open Vector
+open Color
 open System.IO
+
+type Shape = 
+    | Circle of Vector * float
 
 [<EntryPoint>]
 let main args =
+    if Array.length args < 2 then failwith "Not enough arguments"
+    else
     let origin = make (0.0, 0.0, 0.0) // Camera origin
-    let (width, height) as canvas = (640, 480)    
-    let ar = float width / float height
-    let (vw, vh, d) as view = (1. * ar , 1., 1.)    
-    let circleCenter = make (0.0, 0.0, 100.0)
-    let circleRadius = 40.0    
+    let (width, height) as canvas = 
+        (int args.[0], int args.[1])    
+    let (vw, vh, d) as view = 
+        let ar = float width / float height
+        (1. * ar , 1., 1.)    
+    let shapes = [Circle (make (0.0, 0.0, 100.0), 40.0)]
+    let lightSource = make (0.0, 100.0, 100.0)
 
     let discriminant b a c = b * b - 4.0 * a * c
     let solve b a d = ((-b - (sqrt d))/(2.0 * a), (-b + sqrt d)/(2.0 * a))
-
     let circleIntersection a b c r = 
         let d = discriminant b a (c - r * r)
         if d < 0. then (infinity, infinity)
         else solve b a d
-
-    // printf "solve stuff: %f %f\n" x1 x2
-
-    // let (x, y, z) = coord (origin + OV * x1)
-    // printf "x:%f, y:%f, z:%f\n" x y z
-
-    let draw f (width, height) = 
-        let rec drawRec = function
-            | (x, y) when x = width - 1 && y = height - 1 -> f (x, y)
-            | (x, y) when x = width - 1 ->
-                f (x, y)
-                drawRec (0, y + 1)
-            | (x, y) -> 
-                f (x, y)
-                drawRec (x + 1, y)
-        drawRec (0, 0)
-       
 
     let bf = new StreamWriter(new FileStream("img.ppm", FileMode.OpenOrCreate))
 
@@ -42,23 +33,55 @@ let main args =
     bf.WriteLine (System.String.Format ("{0} {1}", width, height))
     bf.WriteLine "255"
     
-    let sendRay (x, y) (tMin, tMax) = 
-        let OV = make ((float x * vw / float width) , (float y * vh / float height), d)
-        let OC = origin - circleCenter
-        let a = OV * OV       
-        let b = 2.0 * OC * OV
-        let c = OC * OC
-        let (x1, x2) = circleIntersection a b c circleRadius
-        let shortest = min x1 x2
-        bf.WriteLine (if tMin <= shortest && shortest <= tMax then "255 0 0" else "0 0 0")
+    let rec sendRay origin target shapes distance =
+        let D = target - origin
+        match shapes with 
+        | []            -> distance
+        | Circle (C, r) :: shapes'  -> 
+            let OC = origin - C
+            let a = D * D
+            let b = 2.0 * OC * D
+            let c = OC * OC
+            let (x1, x2) = circleIntersection a b c r
+            let shortest = min x1 x2
+            sendRay origin target shapes' (min distance shortest)
+
 
     for y in [-height/2 .. (height / 2) - 1] do
         for x in [-width/2 .. (width / 2) - 1] do        
-            sendRay (x, y) (1.0, 1000.0)
+            let V = make ((float x * vw / float width) , (float y * vh / float height), d)
+            let t = sendRay origin V shapes infinity
+            let P = origin + V * t
+            let L = lightSource - P
 
-    // draw sendRay canvas
+            bf.WriteLine (if 1.0 <= t && t <= 10000.0 then string red else string black)
+
     bf.Close ()
     0;;
+
+
+
+    // let sendRay (x, y) (tMin, tMax) = 
+    //     let OV = make ((float x * vw / float width) , (float y * vh / float height), d)
+    //     let OC = origin - circleCenter
+    //     let a = OV * OV       
+    //     let b = 2.0 * OC * OV
+    //     let c = OC * OC
+    //     let (x1, x2) = circleIntersection a b c circleRadius
+    //     let shortest = min x1 x2
+    //     bf.WriteLine (if tMin <= shortest && shortest <= tMax then "255 0 0" else "0 0 0")
+
+    // let draw f (width, height) = 
+    //     let rec drawRec = function
+    //         | (x, y) when x = width - 1 && y = height - 1 -> f (x, y)
+    //         | (x, y) when x = width - 1 ->
+    //             f (x, y)
+    //             drawRec (0, y + 1)
+    //         | (x, y) -> 
+    //             f (x, y)
+    //             drawRec (x + 1, y)
+    //     drawRec (0, 0)
+
 
 
     // printf "%f" (coord myLine origin D x1)
