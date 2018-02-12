6bd39d2d5399e946bc389b57792e38c72b94ade0
Frehaa <frehaa@hotmail.dk>
2018-02-12 20:03:25 +0100

    Finish vector library

--- RayTracerPackage1/Vector.fs
+++ RayTracerPackage1/Vector.fs
@@ -12,9 +12,9 @@ let getY (V(_,y,_)) = y
 let getZ (V(_,_,z)) = z
 let getCoord (V(x,y,z)) = (x, y, z)
 let multScalar a (V(x,y,z)) = V(a * x, a * y, a * z)
-let magnitude (V(x, y, z)) = sqrt (x * x + y * y + z + z)
-let dotProduct (V(x1, y1, z1)) (V(x2, y2, z2)) = x1 * x2 + y1 * y2 + z1 * z2
-let crossProduct = ()
+let magnitude (V(x, y, z)) = sqrt (x * x + y * y + z * z)
+let dotProduct (V(ux, uy, uz)) (V(vx, vy, vz)) = ux * vx + uy * vy + uz * vz
+let crossProduct  (V(ux, uy, uz)) (V(vx, vy, vz)) = V (uy * vz - uz * vy, uz * vx - ux * vz, ux * vy - uy * vx)
 let normalise (V(x,y,z) as v) = let m = magnitude v in V(x/m, y/m, z/m)
 let round (V(x,y,z)) (d:int) = V(System.Math.Round(x,d),System.Math.Round(y,d),System.Math.Round(z,d))
 
@@ -22,7 +22,7 @@ type Vector with
   static member ( ~- ) (V(x,y,z)) = V(-x,-y,-z)
   static member ( + ) (V(ux,uy,uz),V(vx,vy,vz)) = V(ux + vx, uy + vy, uz + vz)
   static member ( - ) (V(ux,uy,uz),V(vx,vy,vz)) = V(ux - vx, uy - vy, uz - vz)
-  static member ( * ) (a, v) = multScalar a v
+  static member ( * ) (s, v) = multScalar s v
   static member ( * ) (v1, v2) = dotProduct v1 v2
-  static member ( % ) ...
-  static member ( / ) ...
+  static member ( % ) (v1, v2) = crossProduct v1 v2
+  static member ( / ) (v, f) = multScalar (1.0/f) v
